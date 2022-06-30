1a14a29936e2ef8739dbe005b316c1c108b7f69c
Vasu021 <amlendushekhar828@gmail.com>
2022-06-30 13:12:53 -0700

    user joined broadcast feature working

--- frontend/client.js
+++ frontend/client.js
@@ -1,9 +1,19 @@
 const socket=io('http://localhost:8000');
-
 const form = document.getElementById('send-form');
 const message = document.getElementById('messageInp');
 const msgContainer = document.querySelector('.container');
 
-const user = prompt("Please enter your name");
+const append = (msg, position) =>{
+    const msgElement = document.createElement('div');
+    msgElement.innerText = msg;
+    msgElement.classList.add(position);
+    msgContainer.appendChild(msgElement);
+}
 
+const user = prompt("Please enter your name");
 socket.emit('new-user-joined', user);
+
+socket.on('user-joined', username=>{
+    append(`${username} joined the chat --------- let's go`, 'msg-right');
+})
+
