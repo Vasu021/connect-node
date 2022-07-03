607842095681aacaae9475c4bef5d350c07bf363
Vasu021 <amlendushekhar828@gmail.com>
2022-07-03 05:52:28 -0700

    game login done

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,12 @@ const io=require('socket.io')(8000);
 
 const users = {};
 
+// words for comparision
+const words = [];
+words[0]="";
+words[1]="";
+var index=0;
+
 io.on('connection', socket=>{
 
     socket.on('new-user-joined', name=>{
@@ -14,7 +20,24 @@ io.on('connection', socket=>{
     });
 
     socket.on('send', message=>{
-        socket.broadcast.emit('recieve', {message: message, name:users[socket.id]})
+        // socket.broadcast.emit('recieve', {message: message, name:users[socket.id]})
+        if(index==0){
+            words[index]=message;
+            index++;
+        }
+        else{
+            words[index]=message;
+            socket.broadcast.emit('recieve', {message: message, name:users[socket.id]})
+            if(words[0].toLowerCase()===words[1].toLowerCase()){
+                io.emit('connected', {word1: words[0], word2: words[1]});
+            }
+            else{
+                io.emit('not-connected', {word1: words[0], word2: words[1]});
+            }
+            index=0;
+            words[0]="";
+            words[1]="";
+        }
     });
 
     socket.on('disconnect', message=>{
--- frontend/client.js
+++ frontend/client.js
@@ -2,6 +2,7 @@ const socket=io('http://localhost:8000');
 const form = document.getElementById('send-form');
 const message = document.getElementById('messageInp');
 const msgContainer = document.querySelector('.container');
+var gameMsg="";
 
 
 // functions ---------------------------------------------------------------------------
@@ -31,15 +32,28 @@ socket.on('recieve', data =>{
     append(`${data.name} :: ${data.message}`,'msg-left');
 });
 
+
 // sent a message
 form.addEventListener('submit', (event)=>{
     event.preventDefault();
+    gameMsg=message.value;
     socket.emit('send', message.value);
     append(`You :: ${message.value}`, 'msg-right');
+    
     message.value="";
 });
 
 // if someone disconnects
 socket.on('user-left', data=>{
     append(`${data} disconnected`, 'user-disconnect');
+});
+
+// if words connected
+socket.on('connected', data=>{
+    append(`====== CONNECTED!!! word::${data.word1} ======`, 'connected');
+});
+
+// if words not connected
+socket.on('not-connected', data=>{
+    append(`${data.word1} <---TRY AGAIN---> ${data.word2}`, 'not-connected');
 });
